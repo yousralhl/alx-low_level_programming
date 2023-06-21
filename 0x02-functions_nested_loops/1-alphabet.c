2b8a0e7a62fb0ffd2319aa16880f39fe75ffc93e
Yousra <lahlaouti.yousra@gmail.com>
2023-06-20 17:03:39 -0700

    nn

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,11 +3,9 @@
  * main - prints the alphabet in lower case follow by a new line
  * 
  */
-
 void print_alphabet(void)
-{
- char letter;
- for (letter = 'a'; letter <= 'z'; letter++)
+{char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
 	_putchar(letter);
- _putchar ('\n');
-}
+_putchar ('\n');}
